d09b161c91c9e26b03a7740533784ddf9780d80b
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-02-27 11:28:31 +0800

    Prepare a component sidebar
    1) Create sidebar component (still empty)
    2) Add sidebar component to 'global' layout

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,47 @@
+<aside>
+  <nav>
+    <ul>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/about">about</a>
+      </li>
+    </ul>
+  </nav>
+</aside>
+
+<style>
+  aside {
+    /* position: absolute; */
+    left: 0%;
+    height: inherit;
+  }
+</style>
