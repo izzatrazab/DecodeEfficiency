2aa932b71ee394f52899d196cbdca9e6dd0fae2a
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-06-09 12:57:50 +0800

    clean up routes\[slug]

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	import type { Post } from '$lib/types.js'
-
 	export let data
-	const post: Post = data.post
 </script>
 
 <svelte:head />
 <article>
 	<h1>
-		{post.title}
+		{JSON.stringify(data.metadata)}
 	</h1>
-	<svelte:component this={data.content} />
+	<svelte:component this={data.component} />
 </article>
--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -1,12 +1,14 @@
+import type { Post } from '$lib/types'
 import { error } from '@sveltejs/kit'
 
 export async function load({ params }) {
 	try {
-		const post = await import(`../../lib/posts/${params.slug}.svelte`)
+		const post: {metadata:Post,default:any} = await import(`../../lib/posts/${params.slug}.svelte`)
+		post.metadata['slug'] = params.slug
 
 		return {
-			content: post.default,
-			post: post.post
+			component : post.default,
+			metadata: post.metadata
 		}
 	} catch (e) {
 		throw error(404, `Could not find ${params.slug}`)
