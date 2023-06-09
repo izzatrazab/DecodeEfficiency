8e17136e5c98d36b653cf126a9d5876878cde7ce
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-06-09 13:00:03 +0800

    category\[category] page and data load

---
+++ src/routes/category/[category]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let data
+</script>
+
+<svelte:head />
+
+<p>{JSON.stringify(data)}</p>
---
+++ src/routes/category/[category]/+page.ts
@@ -0,0 +1,14 @@
+import type { Post } from '$lib/types'
+
+export async function load({ params , fetch}) {
+	const response = await fetch('../../api/posts', {
+		method: 'POST',
+		body: JSON.stringify({
+			category: params.category
+		})
+	})
+
+	const posts: Post[] = await response.json()
+	return {posts}
+	// return { posts }
+}
