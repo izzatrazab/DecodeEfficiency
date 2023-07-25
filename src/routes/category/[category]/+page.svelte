7ef59a41c6d248d66a3fd53094f77b554a059af8
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-07-25 23:45:05 +0800

    prepare post by category page

--- src/routes/category/[category]/+page.svelte
+++ src/routes/category/[category]/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import Card from '$lib/components/card.svelte';
+	import Chips from '$lib/components/chips.svelte'
 	export let data
 </script>
 
 <svelte:head />
 
 <div>
-	{#each  data.posts as post }
-	<Card title={post.title} description={post.description} slug={post.slug}/>
+	{#each data.posts as datum}
+		<Card title={datum.title} description={datum.description} slug={datum.slug} --card-background='red' >
+			<Chips arr={datum.categories}/>
+		</Card>
 	{/each}
 </div>
 
