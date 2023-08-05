b5bbd1501ba796509936980c4da99e5daa5920f5
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-08-05 16:53:48 +0800

    setup shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,30 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite'
-import { mdsvex } from 'mdsvex'
+
+//markdown
+import { mdsvex, escapeSvelte } from 'mdsvex'
 import remarkToc from 'remark-toc'
 import rehypeSlug from 'rehype-slug'
+import shiki from 'shiki';
 
 /**@type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
 	remarkPlugins: [remarkToc],
-	rehypePlugins: [rehypeSlug]
+	rehypePlugins: [rehypeSlug],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await shiki.getHighlighter({ theme: 'dark-plus' })
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\` }`
+		}
+	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(),mdsvex(mdsvexOptions)],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter()
 	},
