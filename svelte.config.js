77c0dca3864c29723ffde5e8b6982800f72a167c
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-09-20 23:28:23 +0800

    remove rehype-slug, remark-toc, and shiki

--- svelte.config.js
+++ svelte.config.js
@@ -2,23 +2,11 @@ import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
 //markdown
-import { mdsvex, escapeSvelte } from 'mdsvex'
-import remarkToc from 'remark-toc'
-import rehypeSlug from 'rehype-slug'
-import shiki from 'shiki';
+import { mdsvex } from 'mdsvex'
 
 /**@type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
 	extensions: ['.md'],
-	remarkPlugins: [remarkToc],
-	rehypePlugins: [rehypeSlug],
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			const highlighter = await shiki.getHighlighter({ theme: 'dark-plus' })
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
-			return `{@html \`${html}\` }`
-		}
-	}
 }
 
 /** @type {import('@sveltejs/kit').Config} */
