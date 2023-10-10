ff9646295d3c58fdc05fa039e50eeecd06f15a05
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-10-10 23:01:52 +0800

    Category page: UI update

--- src/routes/category/+page.svelte
+++ src/routes/category/+page.svelte
@@ -5,9 +5,25 @@
 <svelte:head>
 	<title>Category</title>
 </svelte:head>
+<main>
 {#each categories as category}
-<p>
-    <a href={'category/'+category}>{category}</a>
-</p>
-{/each}
-<p>display lis of category available</p>
+        <a href={'category/'+category}>{category}</a>
+    {/each}
+</main>
+
+<style>
+main {
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+}
+
+a{
+    color: var(--primary-inverse);
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: .75rem;
+    border-radius: 10px;
+    background-color: var(--primary);
+}
+</style>
